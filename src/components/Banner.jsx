51882107dab4432a8be2e95a50fef4138a1b17bd
regone7 <regone12345@gmail.com>
2024-06-07 18:01:26 +0600

    home page search

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,85 @@
 
+import { useEffect, useState } from 'react';
+import useAxiosPublic from '../hook/useAxiosPublic';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
 
 const Banner = () => {
+    // const axiosPublic = useAxiosPublic()
+    // const { data: allcontstsc = [], refetch } = useQuery({
+    //     queryKey: ['allcontstsc'],
+    //     // enabled: !loading && !!user?.email,
+    //     queryFn: async () => {
+    //         const { data } = await axiosPublic.get('/allcontstsearch?search${search}')
+    //         return data;
+
+    //     },
+    // })
+    // const [allcontstsec,setAllcontstsc] = useState([allcontstsc])
+    // console.log(allcontstsec)
+    const [allcontstsec,setAllcontstsec]=useState([])
+    const [search,setSearch]=useState('')
+    const handelSearch = (e) => {
+        e.preventDefault();
+        const searchText = e.target.search.value;
+        setSearch(searchText);
+    }
+
+    useEffect(()=>{
+        fetch(`http://localhost:7000/allcontstsearch?search=${search}`)
+        .then(res=>res.json())
+        .then(data=>setAllcontstsec(data))
+    },[search])
+
+    console.log(allcontstsec)
+
+
+    
     return (
         <div >
             <div className="bg-gradient-to-b from-blue-200 via-orange-100 to-blue-100 h-[450px] flex justify-center items-center">
                 <div className="flex flex-col">
                     <h1 className="text-center text-xl" >Welcome to <p className="btn btn-ghost  text-xl text-white">  Innovate <span className=" text-orange-500">Contest</span> </p> the ultimate platform where innovation meets recognition!</h1>
                     <p className="text-center">Contest Creation Platform</p>
-                    <div className="mt-12 flex  justify-center items-center">
-                        <input type="text" placeholder="Type here" className="input input-bordered input-warning w-50% md:w-full md:max-w-xs  rounded-sm" />
-                        <button className="btn bg-orange-400 hover:bg-orange-500 rounded-sm rounded-r-full text-white ">Search</button>
-                    </div>
+                    <form onSubmit={handelSearch} >
+                        <div className="mt-12 flex  justify-center items-center">
+
+                            <input type="text" name='search' placeholder="Type here" className="input input-bordered input-warning w-50% md:w-full md:max-w-xs  rounded-sm" />
+                            <input className="btn bg-orange-400 hover:bg-orange-500 rounded-sm rounded-r-full text-white " type="submit" value="Search" />
+
+                        </div>
+                    </form>
                 </div>
             </div>
+
+            <div className="container mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-3 gap-3 ">
+                    {
+                        allcontstsec?.slice(0,9).map((allcontstsrc) => (
+                            <div key={allcontstsrc._id} className="card  bg-base-100 shadow-xl image-full h-[310px] md:h-[270px]">
+                                <figure><img src={allcontstsrc.photoURL} alt="" /></figure>
+                                <div className="card-body">
+                                    <div>
+                                        <h2 className="card-title">{allcontstsrc.contest_name}</h2>
+                                        <p>Attempted: <span className="text-red-500 font-bold">{allcontstsrc.attempt}</span></p>
+                                        <p>Contest Type: {allcontstsrc.tags}</p>
+
+                                    </div>
+                                    <p > Description: {allcontstsrc.text_instruction.slice(0, 50)}...</p>
+                                    <div className="card-actions justify-end">
+
+                                        <button className="btn btn-sm  hover:bg-blue-500 hover:text-white">Details</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        ))
+                    }
+                </div>
+            </div>
+
+            
         </div>
     );
 };
--- src/pages/allcontest/AllContest.jsx
+++ src/pages/allcontest/AllContest.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../hook/useAxiosPublic";
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 const AllContest = () => {
     const axiosPublic = useAxiosPublic()
@@ -16,7 +17,7 @@ const AllContest = () => {
     console.log(allcontst)
     return (
         <div>
-            <div className="container mx-auto">
+            {/* <div className="container mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-3 gap-3 ">
                     {
                         allcontst.map((allcontsts) => (
@@ -39,6 +40,215 @@ const AllContest = () => {
 
                         ))
                     }
+                </div>
+            </div> */}
+            <div className='flex justify-center items-center mt-7 mb-12  flex-grow  '>
+                <div className='overflow-x-auto'>
+                    <Tabs>
+                        <TabList>
+                            <Tab ><h1 className='font-semibold text-sm  text-orange-300'>Movie Review</h1></Tab>
+                            <Tab><h1 className='font-semibold text-sm text-orange-300'>Gaming Review</h1></Tab>
+                            <Tab ><h1 className='font-semibold text-sm text-orange-300'>Book Review</h1></Tab>
+                            <Tab><h1 className='font-semibold text-sm text-orange-300'>Business Idea Concerts</h1></Tab>
+                            <Tab><h1 className='font-semibold text-sm text-orange-300'>Marketing Strategy</h1></Tab>
+                            <Tab ><h1 className='font-semibold text-sm text-orange-300'>Article Writing</h1></Tab>
+                            <Tab ><h1 className='font-semibold text-sm text-orange-300'>Digital advertisement Contests</h1></Tab>
+                            <Tab><h1 className='font-semibold text-sm text-orange-300'>Image Design Contests</h1></Tab>
+                        </TabList>
+
+                        <TabPanel>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3">
+                                {
+                                    allcontst.filter(j => j.tags === 'Movie Review').map(tag => (
+                                        <div key={tag._id} className="card  bg-base-100 shadow-xl image-full h-[310px] md:h-[270px]">
+                                            <figure><img src={tag.photoURL} alt="" /></figure>
+                                            <div className="card-body">
+                                                <div>
+                                                    <h2 className="card-title">{tag.contest_name}</h2>
+                                                    <p>Attempted: <span className="text-red-500 font-bold">{tag.attempt}</span></p>
+                                                    <p>Contest Type: {tag.tags}</p>
+
+                                                </div>
+                                                <p > Description: {tag.text_instruction.slice(0, 50)}...</p>
+                                                <div className="card-actions justify-end">
+
+                                                    <button className="btn btn-sm  hover:bg-blue-500 hover:text-white">Details</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3">
+                                {
+                                    allcontst.filter(j => j.tags === 'Gaming Review').map(tag => (
+                                        <div key={tag._id} className="card  bg-base-100 shadow-xl image-full h-[310px] md:h-[270px]">
+                                            <figure><img src={tag.photoURL} alt="" /></figure>
+                                            <div className="card-body">
+                                                <div>
+                                                    <h2 className="card-title">{tag.contest_name}</h2>
+                                                    <p>Attempted: <span className="text-red-500 font-bold">{tag.attempt}</span></p>
+                                                    <p>Contest Type: {tag.tags}</p>
+
+                                                </div>
+                                                <p > Description: {tag.text_instruction.slice(0, 50)}...</p>
+                                                <div className="card-actions justify-end">
+
+                                                    <button className="btn btn-sm  hover:bg-blue-500 hover:text-white">Details</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3">
+                                {
+                                    allcontst.filter(j => j.tags === 'Book Review').map(tag => (
+                                        <div key={tag._id} className="card  bg-base-100 shadow-xl image-full h-[310px] md:h-[270px]">
+                                            <figure><img src={tag.photoURL} alt="" /></figure>
+                                            <div className="card-body">
+                                                <div>
+                                                    <h2 className="card-title">{tag.contest_name}</h2>
+                                                    <p>Attempted: <span className="text-red-500 font-bold">{tag.attempt}</span></p>
+                                                    <p>Contest Type: {tag.tags}</p>
+
+                                                </div>
+                                                <p > Description: {tag.text_instruction.slice(0, 50)}...</p>
+                                                <div className="card-actions justify-end">
+
+                                                    <button className="btn btn-sm  hover:bg-blue-500 hover:text-white">Details</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3">
+                                {
+                                    allcontst.filter(j => j.tags === 'Business Idea Concerts').map(tag => (
+                                        <div key={tag._id} className="card  bg-base-100 shadow-xl image-full h-[310px] md:h-[270px]">
+                                            <figure><img src={tag.photoURL} alt="" /></figure>
+                                            <div className="card-body">
+                                                <div>
+                                                    <h2 className="card-title">{tag.contest_name}</h2>
+                                                    <p>Attempted: <span className="text-red-500 font-bold">{tag.attempt}</span></p>
+                                                    <p>Contest Type: {tag.tags}</p>
+
+                                                </div>
+                                                <p > Description: {tag.text_instruction.slice(0, 50)}...</p>
+                                                <div className="card-actions justify-end">
+
+                                                    <button className="btn btn-sm  hover:bg-blue-500 hover:text-white">Details</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3">
+                                {
+                                    allcontst.filter(j => j.tags === 'Marketing Strategy').map(tag => (
+                                        <div key={tag._id} className="card  bg-base-100 shadow-xl image-full h-[310px] md:h-[270px]">
+                                            <figure><img src={tag.photoURL} alt="" /></figure>
+                                            <div className="card-body">
+                                                <div>
+                                                    <h2 className="card-title">{tag.contest_name}</h2>
+                                                    <p>Attempted: <span className="text-red-500 font-bold">{tag.attempt}</span></p>
+                                                    <p>Contest Type: {tag.tags}</p>
+
+                                                </div>
+                                                <p > Description: {tag.text_instruction.slice(0, 50)}...</p>
+                                                <div className="card-actions justify-end">
+
+                                                    <button className="btn btn-sm  hover:bg-blue-500 hover:text-white">Details</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3">
+                                {
+                                    allcontst.filter(j => j.tags === 'Article Writing').map(tag => (
+                                        <div key={tag._id} className="card  bg-base-100 shadow-xl image-full h-[310px] md:h-[270px]">
+                                            <figure><img src={tag.photoURL} alt="" /></figure>
+                                            <div className="card-body">
+                                                <div>
+                                                    <h2 className="card-title">{tag.contest_name}</h2>
+                                                    <p>Attempted: <span className="text-red-500 font-bold">{tag.attempt}</span></p>
+                                                    <p>Contest Type: {tag.tags}</p>
+
+                                                </div>
+                                                <p > Description: {tag.text_instruction.slice(0, 50)}...</p>
+                                                <div className="card-actions justify-end">
+
+                                                    <button className="btn btn-sm  hover:bg-blue-500 hover:text-white">Details</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3">
+                                {
+                                    allcontst.filter(j => j.tags === 'Digital advertisement Contests').map(tag => (
+                                        <div key={tag._id} className="card  bg-base-100 shadow-xl image-full h-[310px] md:h-[270px]">
+                                            <figure><img src={tag.photoURL} alt="" /></figure>
+                                            <div className="card-body">
+                                                <div>
+                                                    <h2 className="card-title">{tag.contest_name}</h2>
+                                                    <p>Attempted: <span className="text-red-500 font-bold">{tag.attempt}</span></p>
+                                                    <p>Contest Type: {tag.tags}</p>
+
+                                                </div>
+                                                <p > Description: {tag.text_instruction.slice(0, 50)}...</p>
+                                                <div className="card-actions justify-end">
+
+                                                    <button className="btn btn-sm  hover:bg-blue-500 hover:text-white">Details</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3">
+                                {
+                                    allcontst.filter(j => j.tags === 'Image Design Contests').map(tag => (
+                                        <div key={tag._id} className="card  bg-base-100 shadow-xl image-full h-[310px] md:h-[270px]">
+                                            <figure><img src={tag.photoURL} alt="" /></figure>
+                                            <div className="card-body">
+                                                <div>
+                                                    <h2 className="card-title">{tag.contest_name}</h2>
+                                                    <p>Attempted: <span className="text-red-500 font-bold">{tag.attempt}</span></p>
+                                                    <p>Contest Type: {tag.tags}</p>
+
+                                                </div>
+                                                <p > Description: {tag.text_instruction.slice(0, 50)}...</p>
+                                                <div className="card-actions justify-end">
+
+                                                    <button className="btn btn-sm  hover:bg-blue-500 hover:text-white">Details</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </TabPanel>
+                    </Tabs>
                 </div>
             </div>
         </div>
